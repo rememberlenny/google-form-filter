7b73b65f95a1c2e7601a26cd26cac6d42984d1b7
rememberlenny <rememberlenny@gmail.com>
2015-01-21 23:06:38 -0500

    Create code to identify a row of user data, identify user's filter, show relevant questions, and autofill username

--- index.js
+++ index.js
@@ -1,7 +1,172 @@
-var filter = '';
+(function(Tabletop){
 
-var config = {};
+  /**
+   * gff App object
+   */
+  var gff = gff || {};
 
-var questions = {};
+  /**
+   * Config parameters
+   * @type {Object}
+   */
+  gff.config = {
+    "categories":             {
+      // "Abstract":     [1,2],
+      // "Graffiti":     [3,4],
+      // "Photography":  [5,6],
+      // "Classic":      [7,8]
+    },
+    "questions":              [],
+    "userDataRow":            '',
+    "userDriveUrl":           'http://',
+    "filterColumnName":       'Selection',
+    "filterColumnDelimiter":  ',',          // Graffiti, Photography
+    "userColumnName":         'Email',
+    "userParamName":          'email',      // ?email='rememberlenny@gmail.com'
+    "questionWrapper":        'ol.ss-question-list',
+    "questionSelectors":      'ol div.ss-form-question',
+  };
 
-var autopopulate = {};
+  /**
+   * First function to run
+   */
+  gff.initialize = function(){
+    gff.getDriveData();
+  }
+
+  /**
+   * Callback after Drive data is accessed
+   */
+  gff.kickOff = function(){
+    assignUserFieldFromUrl();
+    gff.config.categories = window.categories;
+    gff.config.userValue = gff.getUserFieldFromUrl();
+    gff.assignUserField();
+    gff.getDriveData();
+    gff.getUsersRow();
+    gff.identiyFilters();
+  }
+
+  /**
+   * Use TableTop to get Drive data
+   */
+  gff.getDriveData = function(){
+    var tableTopConfig = {
+      key: gff.config.userDriveUrl,
+      callback: gff.saveDriveData,
+      simpleSheet: true
+    }
+    Tabletop.init( tableTopConfig );
+  }
+
+  /**
+   * Trigger for app and saves Drive data
+   */
+  gff.saveDriveData = function(data, tabletop){
+    gff.config.driveData = data;
+    gff.kickOff();
+  }
+
+  /**
+   * Parse spreadsheet data to find User's data
+   */
+  gff.getUsersRow = function(){
+    for(i = 0; i < gff.config.driveData.length; i++){
+      if(gff.config.driveData[i][gff.config.userColumnName] == gff.config.userValue){
+        gff.config.userDataRow = gff.config.driveData[i];
+      }
+    }
+  }
+
+  /**
+   * Identify the filters in the filterColumn field
+   */
+  gff.identiyFilters = function(){
+    var filterColumn = gff.config.filterColumnName;
+    var delimiter = gff.config.filterColumnDelimiter;
+    var filterColumnValue = gff.config.userDataRow[filterColumn];
+    var filterValues = filterColumnValue.split(delimiter);
+    gff.trimFilterValues(filterValues);
+  }
+
+  /**
+   * Trim the filter values
+   * @param  {[array]} values list of filtered
+   */
+  gff.trimFilterValues = function(values){
+    var filterValues = [];
+    for(i = 0; i < values.length; i++){
+      values[i] = values[i].trim();
+      filterValues = filterValues.push(values[i]);
+    }
+    gff.idenityQuestionSlots(filterValues);
+  }
+
+
+  gff.idenityQuestionSlots = function(filterValues){
+    var slots = [];
+    config.filterValues
+    for(i = 0; i < gff.config.categories.length; i++){
+      if (gff.config.categories[i] == filterValues){
+        slots = slots.concat(filterValues[gff.config.categories[i]]);
+      }
+    }
+    gff.displayQuestions(slots);
+  }
+
+  /**
+   * Store all the questions on form in data
+   */
+  gff.getAllQuestions = function(){
+    gff.config.questions = document.querySelectorAll(gff.config.questionSelectors);
+  }
+
+  /**
+   * Get the user value from URL
+   */
+  gff.getUserFieldFromUrl = function(){
+    var paramValue = getParameterByName(gff.config.userParamName);
+    gff.config.userValue = paramValue;
+  }
+
+  /**
+   * Applies user value to last slot field
+   */
+  gff.assignUserField = function(){
+    var userSlot = gff.config.questions[gff.config.questions.length - 1];
+    var userField = userSlot.querySelector('input');
+    userField.value = gff.config.userValue;
+  }
+
+  /**
+   * Get the query parameters in the URL
+   * @param  {[string]} name Param key
+   * @return {[string]}      Param value
+   */
+  gff.getParameterByName = function(name) {
+      name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+      var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+          results = regex.exec(location.search);
+      return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+
+  /**
+   * Display the questions that are sloted
+   * @param  {[array]} acceptedArray list of visible questions
+   *
+   * This is given an array of slots that should be visible.
+   */
+  gff.displayQuestions = function(acceptedArray){
+    for (i = 0; i < questions.length; i++){
+      for (j = 0; j < acceptedArray.length; j++){
+        if (acceptedArray[j] == i){
+          var el = questions[i];
+          el.classList.add('visible');
+        }
+      }
+    }
+  }
+
+  gff.initialize();
+
+})(Tabletop);
